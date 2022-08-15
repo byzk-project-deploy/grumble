68a28768194e08d6964d57e33b3a3117d3f9b34d
SuLinXin66 <15210204903@163.com>
2022-08-15 13:30:13 +0800

    尝试增加key控制

--- shelltools.go
+++ shelltools.go
@@ -1,12 +1,42 @@
 package grumble
 
-import "github.com/byzk-project-deploy/promptui"
+import (
+	"atomicgo.dev/keyboard"
+	"github.com/byzk-project-deploy/promptui"
+	"github.com/pterm/pterm"
+)
 
 type ShellTools struct {
 	app *App
 }
 
+func (s *ShellTools) keyboardFuncFilterInputRune(r rune) (rune, bool) {
+	_ = keyboard.SimulateKeyPress(r)
+	return 0, false
+}
+
+func (s *ShellTools) keyboardHandle(exit <-chan struct{}) {
+	rawInputRune := s.app.rl.Config.FuncFilterInputRune
+	defer func() { s.app.rl.Config.FuncFilterInputRune = rawInputRune }()
+
+	s.app.rl.Config.FuncFilterInputRune = s.keyboardFuncFilterInputRune
+
+	s.app.rl.Terminal.EnterRawMode()
+	defer s.app.rl.Terminal.ExitRawMode()
+
+	<-exit
+}
+
 func (s *ShellTools) Prompt(prompt *promptui.Prompt) (string, error) {
 	prompt.Readline = s.app.rl
 	return prompt.Run()
+}
+
+func (s *ShellTools) Confirm(label string) (bool, error) {
+	exitChain := make(chan struct{}, 1)
+	defer close(exitChain)
+
+	go s.keyboardHandle(exitChain)
+
+	return pterm.DefaultInteractiveConfirm.Show(label)
 }
